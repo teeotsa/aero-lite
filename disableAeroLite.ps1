3257e6afec96abfad1a137022902d5ac1b165e35
teeotsa <78772453+teeotsa@users.noreply.github.com>
2021-09-12 13:26:42 +0300

    Add files via upload

---
+++ disableAeroLite.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'SilentlyContinue'
+$wshell = New-Object -ComObject Wscript.Shell
+$Button = [System.Windows.MessageBoxButton]::YesNoCancel
+$ErrorIco = [System.Windows.MessageBoxImage]::Error
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Exit
+}
+cls
+$ThemeFolder = "C:\Windows\Resources\Themes"
+Start-Process -FilePath "$ThemeFolder\aero.theme"
+Start-Sleep -Seconds 2
+Remove-Item -Path "$ThemeFolder\aerolite8.theme" -Force | Out-Null
+exit
